886967e95b6cbfdf732a8f1cbe255ebcb24354ba
Vicky Yohnes <59169204+vickyyohanes@users.noreply.github.com>
2019-12-23 20:34:03 +0700

    update main.cpp
    update file main.cpp

--- contoh/main.cpp
+++ contoh/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
 using namespace std;
-
+//int adalah tipe data
+//main adalah
 int main()
 {
-    cout << "Hello world!" << endl;
+   // cout << "Hello world!" << endl;
+    cout << "Hai Vicky!" << endl;
     return 0;
 }
